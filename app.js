609e7e6febcba7eee7d063e4f26fb6869de737eb
Jordan Hunnicutt <41175227+JHunnicutt@users.noreply.github.com>
2018-09-23 18:20:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-document.querySelector('button').onclick = function () {
-  alert("JavaScript is loaded");
-}
-
-document.getElementById('changeText').onclick = function () {
-  document.querySelector('h1').innerHTML = "Heading has been changed";
-}
+"use strict";
